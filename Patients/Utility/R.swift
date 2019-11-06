350f9b7cbbba950c4f7d7caaf2dccca35c2f6963
Jason Tezanos <jason.tezanos@gmail.com>
2019-11-06 09:28:50 -0800

    [R] reuseIdentifier added for TableCell

---
+++ Patients/Utility/R.swift
@@ -0,0 +1,17 @@
+//
+//  R.swift
+//  Patients
+//
+//  Created by Jason Tezanos on 11/6/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import Foundation
+
+/// Faking R.swift
+
+struct R {
+    struct reuseIdentifier {
+        static let tableCell = "TableCell"
+    }
+}
